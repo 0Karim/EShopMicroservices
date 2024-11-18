1746ab13228a30498d2fd8311c686127a9943184
0Karim <karimalaa298@gmail.com>
2024-11-18 10:21:04 +0300

    Modify project to work with .net7 not 8 until upgrade framework

--- src/Services/Catalog/Catalog.API/Products/CreateProductCommandHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProductCommandHandler.cs
@@ -1,11 +1,17 @@
-﻿namespace Catalog.API.Products
+﻿using MediatR;
+
+namespace Catalog.API.Products
 {
 
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     : IRequest<CreateProductResult>;
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand , CreateProductResult>
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
     {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
