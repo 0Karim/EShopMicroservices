e41f37d9cf689ee4e283108f0c02e99f487fd585
karim <karimalaa298@gmail.com>
2025-09-15 09:15:10 +0300

    Implement Order Domain model for microservice according to DDD

---
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Ordering.Domain.Models
+{
+    public class OrderItem : Entity<Guid>
+    {
+        public OrderItem(Guid orderId, Guid productId, int quantity, decimal price)
+        {
+            OrderId = orderId;
+            ProductId = productId;
+            Quantity = quantity;
+            Price = price;
+        }
+
+        public Guid OrderId { private set; get; } = default!;
+
+        public Guid ProductId { private set; get; } = default!;
+
+        public int Quantity { private set; get; } = default!;
+
+        public decimal Price { private set; get; } = default!;
+    }
+}
