9ab5855beccb291fcf0096cd2cf9bccfe19e086e
karim <karimalaa298@gmail.com>
2025-09-28 13:12:37 +0300

    Add Create Order Command for order microservice

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -0,0 +1,13 @@
+﻿using BuildingBlocks.CQRS;
+using Ordering.Application.Dtos;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder
+{
+    public class CreateOrderHandler() : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+    {
+        public Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
