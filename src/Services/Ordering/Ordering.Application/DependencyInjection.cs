8ea2da115c27821538d0c3de370353647592d08b
karim <karimalaa298@gmail.com>
2025-09-29 13:54:28 +0300

    Add log behavior in order microservice

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,14 +1,17 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System.Reflection;
+﻿using System.Reflection;
+using BuildingBlocks.Behaviors;
+using Microsoft.Extensions.DependencyInjection;
 namespace Ordering.Application
 {
     public static class DependencyInjection
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
-            services.AddMediatR(cfg =>
+            services.AddMediatR(config =>
             {
-                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+                config.AddOpenBehavior(typeof(LoggingBehavior<,>));
             });
             return services;
         }
