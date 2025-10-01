9a8d0ae3938366c0443278eab1d840fe31feaef3
karim <karimalaa298@gmail.com>
2025-10-01 10:56:57 +0300

    Implement Create Order Api for order microservice

---
+++ src/Services/Ordering/Orderin.API/Endpoints/UpdateOrder.cs
@@ -0,0 +1,13 @@
+﻿using Ordering.Application.Dtos;
+
+namespace Ordering.API.Endpoints
+{
+    //- Accepts a UpdateOrderRequest.
+    //- Maps the request to an UpdateOrderCommand.
+    //- Sends the command for processing.
+    //- Returns a success or error response based on the outcome.
+
+    public class UpdateOrder
+    {
+    }
+}
