7d89fd2383b6a6ca1d0a8f8b84620218936accc5
karim <karimalaa298@gmail.com>
2025-10-05 14:19:16 +0300

    Implement all order microservice endpoints

--- src/Services/Ordering/Orderin.API/Endpoints/UpdateOrder.cs
+++ src/Services/Ordering/Orderin.API/Endpoints/UpdateOrder.cs
@@ -1,4 +1,5 @@
 ﻿using Ordering.Application.Dtos;
+using Ordering.Application.Orders.Commands.UpdateOrder;
 
 namespace Ordering.API.Endpoints
 {
@@ -7,7 +8,28 @@ namespace Ordering.API.Endpoints
     //- Sends the command for processing.
     //- Returns a success or error response based on the outcome.
 
-    public class UpdateOrder
+    public record UpdateOrderRequest(OrderDto Order);
+    public record UpdateOrderResponse(bool IsSuccess);
+
+    public class UpdateOrder : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/orders", async (UpdateOrderRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<UpdateOrderCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<UpdateOrderResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("UpdateOrder")                    
+            .Produces<UpdateOrderResponse>(StatusCodes.Status200OK)                 
+            .ProducesProblem(StatusCodes.Status400BadRequest)                
+            .WithSummary("Update Order")                
+            .WithDescription("Update Order");
+        }
     }
 }
